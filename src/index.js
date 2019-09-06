fb85de18ca0ac46e0bca58c83549f32628a19057
Dalia <dalia.sawaya@flatironschool.com>
2019-09-06 14:30:22 -0400

    edit index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const endPoint = 'http://localhost:3000/api/v1/coders'
     fetch(endPoint)
         .then(res => res.json())
-        .then (function(){
+        .then (json => {
             console.log('hey!')
             const coder = new Coder(json);
             console.log('hey!')
