e2faa5317757f252ee5ba47787e481141e3aea7c
Dalia <dalia.sawaya@flatironschool.com>
2019-09-10 12:06:22 -0400

    iterated through JSON response from coder index and appended attribute to DOM

--- src/index.js
+++ src/index.js
@@ -4,17 +4,34 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch(endPoint)
         .then(res => res.json())
         .then (json => {
-            console.log('hey!')
-            const coder = new Coder(json);
-            console.log('hey!')
+            json.forEach(function(coder){
+                const newCoder = new Coder(coder)
+                document.getElementById('coders-list').innerHTML = newCoder.name
+                console.log(newCoder.name)
+                //newCoder.formatCoder()
+                //this is just here to confirm the existence of the formatCoder function
+            })
         })
 })
 
 class Coder {
-
     constructor(coder) {
         this.name = coder.name;
         this.language = coder.language;
+        this.hackathons = coder.hackathons;
     }
 
+    formatCoder(){
+        console.log('testing')
+
+    }
+
+
+
 }
+
+//NEXT STEPS
+//list of coders - requires us to create more coders in the db
+//put the rest of the coder's info on the DOM
+//extract all of this functionality into formatCoder so that it is
+//reusable in another context, and call formatCoder on our new coder object
